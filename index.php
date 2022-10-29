af1e220fb325aed6d36e8562892e503c71747ab8
Bryan Siegel <bryansiegel@gmail.com>
2022-10-29 11:29:58 -0700

    basic getting started

--- index.php
+++ index.php
@@ -7,10 +7,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Alpine js</title>
     <script src="//unpkg.com/alpinejs" defer></script>
+
 </head>
 
 <body>
-    
+
+
+    <div x-data="{ open: false }">
+        <button @click="open = true">Expand</button>
+        <div x-show="open" x-cloak>
+            yo
+        </div>
+    </div>
+
 </body>
 
 </html>
